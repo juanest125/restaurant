cecc3572c537e59803e627851ac74ddb17a55e7c
JuanMarquez <juan.marquez@sofka.com.co>
2021-12-15 01:16:45 -0500

    Added new module EntryPoint of type WebFlux.

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/jestma/api/Handler.java
@@ -0,0 +1,15 @@
+package co.com.jestma.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class Handler {
+    public Mono<ServerResponse> listenGETUseCase(ServerRequest serverRequest) {
+        return ServerResponse.ok().bodyValue("");
+    }
+}
